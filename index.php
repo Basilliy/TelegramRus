e163036e707c9bd6ca518be7c4cdd5a3eb7649ba
Basilliy <z.kon2009@gmail.com>
2016-09-19 21:18:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
  * Time: 20:50
  */
 $output = json_decode(file_get_contents('php://input'),true);
-$id = $output['message']['text'];
+$id = $output['message']['chat']['id'];
+file_get_contents("https://api.telegram.org/bot272967076:AAFnC6WbVpExcWWoSXf1TUTE1WlnRiyKLrQ/sendMessage?chat_id=".$id."&text=i can see you");
 file_put_contents("logs.txt",$id);
